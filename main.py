5ab508c8c69b391035da16308fc142af09943d34
Infinityridr <78743681+Infinityridr@users.noreply.github.com>
2025-02-21 17:09:36 -0300

    Update main.py
    made it so the inputs are given after executing the program

--- main.py
+++ main.py
@@ -1,25 +1,77 @@
+KEY = ""
+Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+filename = ""
+
+
 def lowDecypher(char):
-	return Alphabet.lower()[AKEY.lower().index(char)]
+	global Alphabet, KEY
+	return Alphabet.lower()[KEY.lower().index(char)]
 
 def upperDecypher(char):
-	return Alphabet[AKEY.index(char)]
+	global Alphabet, KEY
+	return Alphabet[KEY.index(char)]
 
-text = open("message.txt",'r').read()
-output = ""
+def keyValidate():
+	global KEY
+	if(KEY == ""):
+		KEY = "DECKFMYIQJRWTZPXGNABUSOLVH"
+		return 0
 
-AKEY 	 = "DECKFMYIQJRWTZPXGNABUSOLVH" #PLACE THE ENCRYPTED ALPHABET HERE
-Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	if(not(KEY.isalpha())):
+		print("Key is not Letters.")
+		exit()
 
-for char in text:
+	if(KEY.islower()):
+		KEY = KEY.upper()
+
+	return 0
+
+def textValidate():
+	global filename
+	if (filename == ""):
+		filename = "message.txt"
+
+	try:
+		f = open(filename,"r")
+		f.close()
+		
+	except IOError:
+		print("cannot access text file")
+		exit()
+
+	return 0
+
+def setup():
+	global KEY, filename
+	output = ""
+
+	filename = input("Name of text file to decrypt (Ex: text.txt) (same directory as program)")
+	KEY = input("The decrytion key (Ex: DECKFMYIQJRWTZPXGNABUSOLVH)")
+
+	keyValidate()
+	textValidate()
+
+def decrypt():
+	global filename
+
+	output = ""
+
+	with open(filename,"r") as f:
+		text = f.read()
+
+	for char in text:
 	
-	if(not(char.isalpha())):
-		output += char
-		continue
+		if(not(char.isalpha())):
+			output += char
+			continue
 
-	if(char.islower()):
-		output += lowDecypher(char)
-		continue
+		if(char.islower()):
+			output += lowDecypher(char)
+			continue
 
-	output += upperDecypher(char)
+		output += upperDecypher(char)
 
-print(output)
+	print(output)
+
+setup()
+decrypt()
